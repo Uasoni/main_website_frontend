26bcdaaab7230361c072e18fb5f458ac36b33504
Sunny Chen <sunnychenaustralia@gmail.com>
2026-01-01 17:13:44 +1100

    First commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import './layout.css';
+	import CPMSoc_Logo from '$lib/assets/CPMSoc_Logo.png';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+    <title>UNSW CPMSoc</title>
+    <link rel="icon" href={CPMSoc_Logo} />
+</svelte:head>
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Welcome to CPMSoc website for s3 bucket new one</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>

f53c132e391c16cca66ef9d81c6498ebeb4ff4c9
pat <73502164+nyapat@users.noreply.github.com>
2026-03-18 12:18:23 +1100

    feat: add contest static page

---
+++ src/routes/contest/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+</script>
+
+<div class="max-w-[1000px] mx-auto gap-4 flex flex-col py-6 px-10">
+
+    <h1 class="text-5xl font-bold text-[#5b8ddb]">
+        Contest
+    </h1>
+
+    <p class="text-xl leading-relaxed">
+        This page is currently under maintenance.
+    </p>
+
+</div>
